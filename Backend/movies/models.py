8f27d7277d86f9e6fc2b52b6beb2b700fabc4586
JGrafmiller <jdg66796@uga.edu>
2025-09-26 11:40:31 -0400

    Update models.py
    Updated Movie Model to align with DB

--- Backend/movies/models.py
+++ Backend/movies/models.py
@@ -1,27 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 
 class Movie(models.Model):
     """Model for storing movie information from OMDB API"""
-    title = models.CharField(max_length=255)
-    year = models.CharField(max_length=4)
-    imdb_id = models.CharField(max_length=20, unique=True)
-    plot = models.TextField(blank=True)
-    poster_url = models.URLField(blank=True)
-    genre = models.CharField(max_length=255, blank=True)
-    director = models.CharField(max_length=255, blank=True)
-    actors = models.TextField(blank=True)
-    runtime = models.CharField(max_length=20, blank=True)
-    imdb_rating = models.CharField(max_length=10, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    id = models.AutoField(primary_key= True)
+    title = models.TextField(db_column= 'Title')
+    synopsis = models.TextField(db_column= 'Synopsis')
+    reviews = models.TextField(blank=True, null=True, db_column= 'Reviews')
+    trailer_url = models.URLField(blank=True, null=True, db_column='TrailerURL')
+    trailer_pic_url = models.URLField(blank=True, null=True, db_column='TrailerPicLink')
+    mpaa_rating = models.CharField(max_length=10, db_column='MPAA_US_FILM_RATING')
+    is_running = models.BooleanField(default=False, db_column='isRunning')
+    is_coming_soon = models.BooleanField(default=False, db_column='isComingSoon')
+    director = models.TextField(blank=True, null=True, db_column='Director')
+    producer = models.TextField(blank=True, null=True, db_column='Producer')
+    cast = models.TextField(blank=True, null=True, db_column='Cast')
+    category = ArrayField(models.TextField(), blank=True, null=True, db_column='Category')
+    poster_url = models.URLField(blank=True, null=True, db_column='Poster_img_URL')
+
+
+"""
+Can implement ordering and __str__ later if needed
 
     def __str__(self):
         return f"{self.title} ({self.year})"
 
     class Meta:
         ordering = ['-created_at']
+"""
 
 
 class UserFavorite(models.Model):
@@ -36,7 +44,6 @@ class UserFavorite(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.movie.title}"
-
 
 class MovieReview(models.Model):
     """Model for storing user reviews of movies"""
