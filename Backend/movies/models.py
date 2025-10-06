a81451391210039469819f4d53be78b2d89ab22f
JGrafmiller <jdg66796@uga.edu>
2025-10-06 09:53:44 -0400

    Updated Models and defined methods for retrieving related objects

--- Backend/movies/models.py
+++ Backend/movies/models.py
@@ -53,71 +53,133 @@ class MovieReview(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.movie.title} ({self.rating}/5)"
+ 
 
- # Models for showtimes, seats, booking, users  
+
+ # Models for cinemas, showrooms,showtimes, seats, booking, users
 """
-# Model for storing shows for specific movies
+# Model for Cinema
 
-class MovieShow(models.Model):
-    show_id = models.AutoField(primary_key=True)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    showtime = models.DateTimeField()
-    is_available = models.BooleanField(default = True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    auditorium = models.IntegerField()
+class Cinema(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.IntegerField(max_length=5) # Zip Code 
 
+
+    def get_show_rooms(self):
+        return MovieShow.objects.filter(cinema=self)
+
+    def get_now_showing(self):
+        shows = MovieShow.objects.filter(cinema=self, is_now_showing = True)
+        movies = [show.movie for show in shows]
+        return movies
     
-    #theater = models.TextField() 
+# Model for ShowRoom
 
-    class Meta:
-        ordering = ['-created_at']
-        db_table = 'showtime_table'
-    
-    def __str__(self):
-        return f"Show ID: {self.show_id} {self.movie} ({self.showtime})"
+class ShowRoom(models.Model):
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+    capacity = models.IntegerField()
+    room_number = models.IntegerField()
+
+
 # Model for Seats
 
 class Seat(models.Model):
     show = models.ForeignKey(MovieShow, on_delete=models.CASCADE)
     index = models.IntegerField()
     is_available = models.BooleanField(default=True)
-    seat_type = models.CharField(max_length=1, choices =[('A', 'Adult'), ('C', 'Child'), ('S', 'Senior')])
-    row = models.CharField(max_length=2)
-    number = models.IntegerField()
+    number = models.CharField(max_length=2) # row + number
 
     class Meta: 
         unique_together = ('show', 'index')
-        abstract = True
+ 
+   
+       
+# Model for MovieShow
+
+class MovieShow(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+    showtime = models.DateTimeField()
+    is_now_showing = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    auditorium = models.IntegerField()
+
+
+    class Meta:
+        ordering = ['-created_at']
+        db_table = 'showtime_table'
+
+    # print method
+    def __str__(self):
+        return f"Show ID: {self.show_id} {self.movie} ({self.showtime})"
+
+    # returns listt of seat objects that are available
+    def get_available_seats(self):
+        seats = Seat.objects.filter(show=self, is_available = True)
+        return seats
+    
+
+        
 
 # Model for Booking
 
 class Booking(models.Model):
     id = models.UUIDField(primary_key=True, default= uuid.uuid4, editable=False)
     show = models.ForeignKey(MovieShow, on_delete=models.SET_NULL, null = True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     total_price = models.DecimalField(max_digits=6, decimal_places=2)
     seats = models.ManyToManyField(Seat)
     booking_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         unique_together = ('user', 'id')
-        db_table = 'booking_table'
+        db_table = 'booking'
     
 
+    def reserve_seats(self, seat_list):
 
-# extension of Django's User class, added phone number and favorite movie fields 
-class CustomUser(AbstractUser):
+       # Reserve the given seats for this booking
+     #  Updates availability and show is_full field
+     
+        # Update seat table
+        for seat in seat_list:
+            if not seat.is_available:
+                raise ValueError(f"Seat {seat.number} is unavailable")
+            seat.is_available = False
+            seat.save()
+        
+        # Add seats to booking
+        self.seats.add(*seat_list)
+
+        # Check show is_full
+        show = self.show
+        if not show.seat_set.filter(is_available=True).exists():
+            show.is_full = True
+        else:
+            show.is_full = False
+        show.save()
+    
+    def return_seats(self, seat_list):
+         # Update seat table
+        for seat in seat_list:
+            if not seat.is_available:
+                raise ValueError(f"Seat {seat.number} is unavailable")
+            seat.is_available = True
+            seat.save()
+    
+
+# Customer Model builds off Django's User model to include payment cards and 
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
     phone_number = models.CharField(max_length=15)
-    favorite_movie = models.CharField(max_length=200)
+    address = models.
 
-    
-    def __str__(self):
-        return f"{self.user.username}'s profile"
-    
 # Payment card Model  
 class PaymentCard(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='cards')
     cardholder_name = models.CharField(max_length=100)
     card_number = models.CharField(max_length=16)
     expiration_date = models.DateField()
@@ -129,6 +191,11 @@ class PaymentCard(models.Model):
 
 
 """
+
+
+
+
+
 
 
 
